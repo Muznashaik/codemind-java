d43ecb971a73604117eeee7184d429f09acdc60c
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-09-26 21:17:59 +0530

    import java.util.Scanner; class S {     public static void main(String args[])     {        Scanner sc=new Scanner(System.in);        int a=sc.nextInt();        int[] arr=new int[a+1];        for (int i=1; i<a+1; i++)        {            arr[i]=sc.nextInt();        }        int f=1;        for (i=1; i<a+1; i++)        {            if (arr[i]>=a)            {                f=0;                break;            }        }        if (f==1){            System.out.print("YES");        }        else{            System.out.print("NO");        }     } }

---
+++ Can_Robber_Escape?.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class S
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int[] arr=new int[a+1];
+       for (int i=1; i<a+1; i++)
+       {
+           arr[i]=sc.nextInt();
+       }
+       int f=1;
+       for (i=1; i<a+1; i++)
+       {
+           if (arr[i]>=a)
+           {
+               f=0;
+               break;
+           }
+       }
+       if (f==1){
+           System.out.print("YES");
+       }
+       else{
+           System.out.print("NO");
+       }
+    }
+}

3ce902947626601cdb6b246154ead23e23ada162
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-09-26 21:52:49 +0530

    import java.util.Scanner; class M {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         String M;         M=s.nextLine();         System.out.print(M);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class M
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String M;
+        M=s.nextLine();
+        System.out.print(M);
+    }
+}

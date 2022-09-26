f440f8c99d077b3f4dbad3e65873ac8cb59baf10
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-09-26 23:37:40 +0530

    import java.util.Scanner; class hcf {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=0,h=0,i;         for(i=1;i<=b;i++)         {             if(a%i==0 && b%i==0)             {                 c=1;                 if(c==1)                 {                     h=i;                 }             }         }         System.out.println((int)(a*b)/h);     } }

---
+++ LCM.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class hcf
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0,h=0,i;
+        for(i=1;i<=b;i++)
+        {
+            if(a%i==0 && b%i==0)
+            {
+                c=1;
+                if(c==1)
+                {
+                    h=i;
+                }
+            }
+        }
+        System.out.println((int)(a*b)/h);
+    }
+}

666705592217fe44cf4d317724ff67df74601db0
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-09-29 09:51:18 +0530

    import java.util.Scanner; class classname {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,temp;     n=sc.nextInt();     temp=n;     for(i=1;i<n;i++)     {         if(n%i==0)         {             s=s+i;         }     }     if(s==temp)     {         System.out.println("True");     }     else     {         System.out.println("False");     }     } }

---
+++ Perfect_Number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class classname
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,temp;
+    n=sc.nextInt();
+    temp=n;
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            s=s+i;
+        }
+    }
+    if(s==temp)
+    {
+        System.out.println("True");
+    }
+    else
+    {
+        System.out.println("False");
+    }
+    }
+}

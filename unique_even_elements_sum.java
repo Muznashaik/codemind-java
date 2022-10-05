d288dba2d24a1c7152bf2d9aef50b8e3c837591d
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-10-05 20:58:40 +0530

    import java.util.Scanner; class classname {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],i,j,c=0,k=0;     n=sc.nextInt();     arr=new int[1000];     for(i=0;i<n;i++)     {         arr[i]=sc.nextInt();     }     for(i=0;i<n;i++)     {         c=0;         for(j=i;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         if(c==1)         {             if(arr[i]%2==0)             {                 k=k+arr[i];             }         }     }     System.out.println(k);     } }

---
+++ unique_even_elements_sum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class classname
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,arr[],i,j,c=0,k=0;
+    n=sc.nextInt();
+    arr=new int[1000];
+    for(i=0;i<n;i++)
+    {
+        arr[i]=sc.nextInt();
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            if(arr[i]%2==0)
+            {
+                k=k+arr[i];
+            }
+        }
+    }
+    System.out.println(k);
+    }
+}

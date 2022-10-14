5ca71df4436fefba807c503947a5a04bd6e9b6a1
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-10-14 23:52:47 +0530

    import java.util.Scanner; class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,m,i,c=0;         n=sc.nextInt();         m=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(x[i]%m!=0)             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,m,i,c=0;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%m!=0)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
